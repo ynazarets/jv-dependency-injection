c9cbf76b71eb920b2e5561c9a8e376675f6fe285
Yehor Nazarets <nazarets1111@gmail.com>
2025-09-01 18:17:17 +0200

    Fixed duplicated data injectors

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -2,7 +2,6 @@ package mate.academy.lib;
 
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import mate.academy.service.FileReaderService;
@@ -14,6 +13,10 @@ import mate.academy.service.impl.ProductServiceImpl;
 
 public class Injector {
     private static final Injector injector = new Injector();
+    private static final Map<Class<?>, Class<?>> interfaceImplementations = Map.of(
+            FileReaderService.class, FileReaderServiceImpl.class,
+            ProductParser.class, ProductParserImpl.class,
+            ProductService.class, ProductServiceImpl.class);
     private Map<Class<?>, Object> instances = new HashMap<>();
 
     public static Injector getInjector() {
@@ -21,52 +24,44 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        Object clazzImplementationInstance = null;
         Class<?> clazz = findImplementation(interfaceClazz);
-        if (!(clazz.isAnnotationPresent(Component.class))) {
-            throw new RuntimeException("It is not a Component Class.");
+        if (instances.containsKey(clazz)) {
+            return instances.get(clazz);
         }
+        if (!(clazz.isAnnotationPresent(Component.class))) {
+            throw new RuntimeException("Missing @Component statement in "
+                    + clazz.getName()
+                    + " class");
+        }
+        Object clazzImplementationInstance = createNewInstance(clazz);
+        instances.put(clazz, clazzImplementationInstance);
         Field[] declaredFields = clazz.getDeclaredFields();
         for (Field field : declaredFields) {
             if (field.isAnnotationPresent(Inject.class)) {
                 Object fieldInstance = getInstance(field.getType());
-                clazzImplementationInstance = createNewInstance(clazz);
                 try {
                     field.setAccessible(true);
                     field.set(clazzImplementationInstance, fieldInstance);
                 } catch (IllegalAccessException e) {
                     throw new RuntimeException("Can`t initialize field value."
-                            + "Class; " + clazz.getName() + ". Field: " + field.getName());
+                            + "Class; " + clazz.getName() + ". Field: " + field.getName() + e);
                 }
             }
-        }
-        if (clazzImplementationInstance == null) {
-            clazzImplementationInstance = createNewInstance(clazz);
         }
         return clazzImplementationInstance;
     }
 
     private Object createNewInstance(Class<?> clazz) {
-        if (instances.containsKey(clazz)) {
-            return instances.get(clazz);
-        }
         try {
             Constructor<?> constructor = clazz.getConstructor();
-            Object instance = constructor.newInstance();
-            instances.put(clazz, instance);
-            return instance;
-        } catch (NoSuchMethodException | IllegalAccessException
-            | InstantiationException | InvocationTargetException e) {
+            return constructor.newInstance();
+        } catch (ReflectiveOperationException e) {
             throw new RuntimeException("Can`t create a new instance of "
-                    + clazz.getName());
+                    + clazz.getName() + " " + e);
         }
     }
 
     private Class<?> findImplementation(Class<?> interfaceClazz) {
-        Map<Class<?>, Class<?>> interfaceImplementations = new HashMap<>();
-        interfaceImplementations.put(FileReaderService.class, FileReaderServiceImpl.class);
-        interfaceImplementations.put(ProductParser.class, ProductParserImpl.class);
-        interfaceImplementations.put(ProductService.class, ProductServiceImpl.class);
         if (interfaceClazz.isInterface()) {
             return interfaceImplementations.get(interfaceClazz);
         }
